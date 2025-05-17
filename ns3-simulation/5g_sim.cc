5ffc32d2d8cf8ce0a8fac1d1799592eecac4822b
xingmei <dingxingmei.dxm@gmail.com>
2025-05-17 21:11:18 +0100

    5g_sim: add EPC/RemoteHost, Ip configureation and UDP Downstreaming

--- ns3-simulation/5g_sim.cc
+++ ns3-simulation/5g_sim.cc
@@ -78,6 +78,68 @@ int main(int argc, char *argv[])
 
     NS_LOG_UNCOND("5G NR minimal scenario started!");
 
+        // Create RemoteHost and install protocol stack
+    Ptr<Node> pgw = epcHelper->GetPgwNode();
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create(1);
+    Ptr<Node> remoteHost = remoteHostContainer.Get(0);
+    InternetStackHelper internetRemote;
+    internetRemote.Install(remoteHostContainer);
+
+    // Configure point-to-point link between RemoteHost and PGW
+    PointToPointHelper p2ph;
+    p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
+    p2ph.SetDeviceAttribute("Mtu", UintegerValue(2500));
+    p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+    NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
+
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase("1.0.0.0", "255.0.0.0");
+    Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign(internetDevices);
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting =
+        ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
+    remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
+
+    // Install IP stack on UEs and assign IP addresses
+    Ipv4InterfaceContainer ueIpIface;
+    ueIpIface = epcHelper->AssignUeIpv4Address(NetDeviceContainer(ueDevs));
+
+    // Set default gateway for UEs
+    for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
+    {
+        Ptr<Node> ueNode = ueNodes.Get(u);
+        Ptr<Ipv4StaticRouting> ueStaticRouting =
+            ipv4RoutingHelper.GetStaticRouting(ueNode->GetObject<Ipv4>());
+        ueStaticRouting->SetDefaultRoute(epcHelper->GetUeDefaultGatewayAddress(), 1);
+    }
+
+    // Configure UDP downlink application (RemoteHost -> UE)
+    uint16_t dlPort = 1234;
+    ApplicationContainer clientApps;
+    ApplicationContainer serverApps;
+    for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
+    {
+        // Install UDP server on UE
+        UdpServerHelper dlPacketSinkHelper(dlPort);
+        serverApps.Add(dlPacketSinkHelper.Install(ueNodes.Get(u)));
+
+        // Install UDP client on RemoteHost
+        UdpClientHelper dlClientHelper(ueIpIface.GetAddress(u), dlPort);
+        dlClientHelper.SetAttribute("Interval", TimeValue(MicroSeconds(1000)));
+        dlClientHelper.SetAttribute("MaxPackets", UintegerValue(1000000));
+        dlClientHelper.SetAttribute("PacketSize", UintegerValue(1500));
+        clientApps.Add(dlClientHelper.Install(remoteHost));
+    }
+
+    // Start and stop applications
+    serverApps.Start(Seconds(0.4));
+    clientApps.Start(Seconds(0.4));
+    serverApps.Stop(Seconds(10.0));
+    clientApps.Stop(Seconds(9.8));
+
+
     Simulator::Stop(Seconds(10.0));
     Simulator::Run();
     Simulator::Destroy();
