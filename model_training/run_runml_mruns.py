3b846713eb037012168028a2b5d63c27530323ed
xingmei <dingxingmei.dxm@gmail.com>
2025-07-27 19:01:47 +0100

    change run_ml

--- model_training/run_runml_mruns.py
+++ model_training/run_runml_mruns.py
@@ -6,9 +6,15 @@ parser.add_argument("--folder_path", help="Path to folder with files")
 parser.add_argument("--output_path", help="Path where to save transformed files")
 
 
-runs = [1,2,3,4,5]
+# runs = [1,2,3,4,5]
 
-cmps = [10, 50]
+# cmps = [10, 50]
+
+runs = [1]
+cmps = [100]
+
+# runs = [1,2,3,4,5,6,7,8,9,10]
+# cmps = [10, 50]
 
 
 history = 5
@@ -24,6 +30,6 @@ if __name__ == "__main__":
       input_folder = os.path.join(args.folder_path, "Run_%d"%run + "/CMP%d"%cmp_)
       name = args.output_path.strip("/") + "_H%dH%d"%(history, horizon)
       final_out_name = os.path.join(name, "Run_%d"%run + "/CMP%d"%cmp_)
-      os.system("python run_ml.py --remove_features PDCP_Throughput --folder_path %s --output_path %s --target_metric THR --type raw --scen_name CMP%d --gran 1s"%(input_folder, final_out_name, cmp_))
+      os.system("python model_training/run_ml.py --remove_features PDCP_Throughput --folder_path %s --output_path %s --target_metric THR --type raw --scen_name CMP%d --gran 1s"%(input_folder, final_out_name, cmp_))
 
 # 
